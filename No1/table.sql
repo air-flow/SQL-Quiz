1122fadab4ac7f6d8a64d8332a0bf80b32036e77
updraft <updraft.1970@gmail.com>
2021-10-28 11:59:18 +0900

    table add

---
+++ No1/table.sql
@@ -0,0 +1,35 @@
+CREATE TABLE users(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(10),
+    address char(10)
+);
+
+INSERT INTO
+    users(name, address)
+VALUES
+    ("AAA", "AAA");
+
+INSERT INTO
+    users(name, address)
+VALUES
+    ("BBB", "BBBBBB");
+
+INSERT INTO
+    users(name, address)
+VALUES
+    ("CCCC", "CCC");
+
+INSERT INTO
+    users(name, address)
+VALUES
+    ("aaa", "aaa");
+
+INSERT INTO
+    users(name, address)
+VALUES
+    ("bbb", "bbbbbb");
+
+INSERT INTO
+    users(name, address)
+VALUES
+    ("cccc", "ccc");
