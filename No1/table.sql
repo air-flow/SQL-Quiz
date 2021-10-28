ae0fc3f57846a7e95699016c99f061284a6bb063
updraft <updraft.1970@gmail.com>
2021-10-28 19:06:53 +0900

    city add

--- No1/table.sql
+++ No1/table.sql
@@ -7,7 +7,7 @@ CREATE TABLE users(
 INSERT INTO
     users(name, address)
 VALUES
-    ("AAA", "AAA");
+    ("AAA", "aaa");
 
 INSERT INTO
     users(name, address)
@@ -33,3 +33,46 @@ INSERT INTO
     users(name, address)
 VALUES
     ("cccc", "ccc");
+
+CREATE TABLE city(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(10),
+    CountryCode char(3),
+    Population int
+);
+
+INSERT INTO
+    city(name, CountryCode, Population)
+VALUES
+    ("JP", "AAA", 100);
+
+INSERT INTO
+    city(name, CountryCode, Population)
+VALUES
+    ("Kabul", NULL, 77777);
+
+INSERT INTO
+    city(name, CountryCode, Population)
+VALUES
+    ("Herat", "BBB", 123456789);
+
+SELECT
+    *
+FROM
+    users AS t1
+WHERE
+    t1.name IN (
+        SELECT
+            t2.address
+        FROM
+            users AS t2
+    );
+
+SELECT
+    *
+FROM
+    users
+WHERE
+    name IN ("a", "aa", "aaa")
+    OR id = 2
+    AND mod(id, 2) = 1;
